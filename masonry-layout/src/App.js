db4fb67b81365c2bf6a5319aa1d44efcb9133a20
jrtwheeler <jwheele1@gmail.com>
2021-09-18 23:58:39 -0400

    Added images

--- masonry-layout/src/App.js
+++ masonry-layout/src/App.js
@@ -3,11 +3,19 @@ import './App.css';
 
 function App() {
   const [images, setImages] = useState ([
-    {url: "https://source.unsplash.com/random/1"}
+    {url: "https://source.unsplash.com/random/1"},
+    {url: "https://source.unsplash.com/random/2"},
+    {url: "https://source.unsplash.com/random/3"},
+    {url: "https://source.unsplash.com/random/4"},
+    {url: "https://source.unsplash.com/random/5"},
+    {url: "https://source.unsplash.com/random/6"}
   ])
   
   return (
     <div className="App">
+      <div className="App">
+        
+      </div>
       
     </div>
   );
